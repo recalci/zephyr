4bd46d55d05cdb2d62d2c45afea59250adc3304f
Jianxiong Gu <jianxiong.gu@outlook.com>
2025-03-21 12:05:36 +0800

    boards: Introduce WeAct bluepill_ch32v203
    Introduce WeAct Blue Pill based on CH32V203
    
    Signed-off-by: Jianxiong Gu <jianxiong.gu@outlook.com>

---
+++ boards/weact/bluepill_ch32v203/board.cmake
@@ -0,0 +1,5 @@
+# Copyright (c) 2025 Jianxiong Gu <jianxiong.gu@outlook.com>
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(minichlink)
+include(${ZEPHYR_BASE}/boards/common/minichlink.board.cmake)
